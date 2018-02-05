7d38bfc779e886a8cfb62d8e48da3ec80724f63d
houzhongjian <houzhongjian@kxkr.com>
2018-02-05 15:26:28 +0800

    init

--- db/db.go
+++ db/db.go
@@ -1,6 +1,25 @@
 package db
 
+import (
+	"fmt"
+	"log"
+
+	cf "github.com/houzhongjian/jiancheng/conf"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var Db *gorm.DB
+
 //DbInit 初始化数据库.
 func DbInit() {
+	args := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cf.Conf.DbHost, cf.Conf.DbPort, cf.Conf.DbUser, cf.Conf.DbPassword, cf.Conf.DbName, "disable")
+	db, err := gorm.Open(cf.Conf.DbUser, args)
+	if err != nil {
+		log.Printf("%+v\n", err)
+		return
+	}
+	db.SingularTable(true)
 
+	Db = db
 }
