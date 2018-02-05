ac8672490bcba762869ed87f26b9267074335ac4
houzhongjian <houzhongjian@kxkr.com>
2018-02-05 13:58:37 +0800

    index

--- js/main.js
+++
@@ -1,9 +0,0 @@
- $(function(){
-	$(".hgroup a").click(function(){
-	  $(this).siblings().removeClass('active');
-	  $(this).addClass('active');
-	  $('.listss div').removeClass('show');
-      $('.listss div').eq($(this).index()).addClass('show');
-   })
-
-});
--- main.go
+++ main.go
@@ -8,15 +8,15 @@ import (
 
 func main() {
 	http.HandleFunc("/", HandleDefault)
-	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
-	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./js"))))
-	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./images"))))
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./frontend/css"))))
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./frontend/js"))))
+	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./frontend/images"))))
 	http.ListenAndServe(":8080", nil)
 }
 
 //HandleDefault .
 func HandleDefault(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("./index.html")
+	t, err := template.ParseFiles("./frontend/index.html")
 	if err != nil {
 		log.Printf("%+v\n", err)
 		return
